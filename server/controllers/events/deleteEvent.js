36862f305017e71f60d5fcdf43e6acf42b40edf0
phemy <phemy.smith@gmail.com>
2017-11-17 22:29:59 +0100

    refactored delete event logic

--- server/controllers/events/deleteEvent.js
+++ server/controllers/events/deleteEvent.js
@@ -3,7 +3,14 @@ const data = db.events;
 
 class DeleteEvent{
     static delete(req, res){
-        res.status(200).send('deleted')
+        let { id } = req.params
+        for(let key=0; key<data.length; key++){
+			if(data[key].id === id){
+            	data.splice(key,1);
+                res.status(200).json(data)
+             }
+         }
+        res.status(404).send('unknown event cant be deleted')
     }
 }
 export default DeleteEvent;
